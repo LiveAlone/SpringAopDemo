8304fa382cf464729ae404beedf816e4d5bab32d
qijunyao <qijunyao@xiaohongshu.com>
2018-09-10 12:52:51 +0800

    add non

--- src/main/java/org/yqj/aop/demo/RunCommandLine.java
+++ src/main/java/org/yqj/aop/demo/RunCommandLine.java
@@ -3,6 +3,7 @@ package org.yqj.aop.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.yqj.aop.demo.service.TeacherService;
 
@@ -18,6 +19,9 @@ public class RunCommandLine implements CommandLineRunner{
 
     @Autowired
     private TeacherService teacherService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     public void run(String... args) throws Exception {
         log.info("teacher service get teacher name :{}", teacherService.gainTeacherName());
--- src/main/java/org/yqj/aop/demo/adaptor/LocalInstanceBeanPostProcessor.java
+++ src/main/java/org/yqj/aop/demo/adaptor/LocalInstanceBeanPostProcessor.java
@@ -1,21 +1,44 @@
-package org.yqj.aop.demo.spring.adapt;
+package org.yqj.aop.demo.adaptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValues;
-import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
 import org.springframework.stereotype.Component;
 import org.yqj.aop.demo.service.TeacherService;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Constructor;
 
 /**
  * @author yaoqijun on 2018-04-08.
  */
 @Component
 @Slf4j
-public class LocalInstanceBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+public class LocalInstanceBeanPostProcessor implements SmartInstantiationAwareBeanPostProcessor, BeanFactoryAware {
+
+    public LocalInstanceBeanPostProcessor(){
+        log.info("construct time");
+    }
+
+    @Override
+    public Class<?> predictBeanType(Class<?> beanClass, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Constructor<?>[] determineCandidateConstructors(Class<?> beanClass, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object getEarlyBeanReference(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
     @Override
     public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
         if (beanClass.equals(TeacherService.class)){
@@ -54,5 +77,10 @@ public class LocalInstanceBeanPostProcessor implements InstantiationAwareBeanPos
             log.info("******* teacher service after init");
         }
         return bean;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        log.info("bean factory is in hahah");
     }
 }
